54a70424a921f89a3a4559a12b54758c01534d6b
MaranathaOkeleyOdai <moodai@st.ug.edu.gh>
2024-08-30 13:13:00 +0000

    created the services and crud operations for coffee services

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,11 +1,40 @@
 import { Controller,
             Get,
+            Param,
+            Post,
+            Body,
+            HttpCode,
+            HttpStatus,
+            Res,
+            Patch,
+            Delete,
+            Query
      } from '@nestjs/common';
+import { CoffeesService } from './coffees.service';     
 
 @Controller('coffees')
 export class CoffeesController {
+    constructor(private readonly CoffeesService: CoffeesService){}
     @Get()
-    findAll() {
-        return 'This action returns all coffees';
+
+    findAll(@Query()paginationQuery) {
+       // const {limit , offset} = paginationQuery;
+        return this.CoffeesService.findAll();
+    }
+    @Get(':id')
+    findOne(@Param('id') id:string){
+        return this.CoffeesService.findOne(id);
+    }
+    @Post()
+    create(@Body() body){
+        return this.CoffeesService.create(body);
+    }
+    @Patch(':id')
+    update(@Param('id')id:string, @Body() body){
+        return this.CoffeesService.update(id, body);
+    }
+    @Delete(':id')
+    remove(@Param('id')id:string){
+        return this.CoffeesService.remove(id);
     }
 }
